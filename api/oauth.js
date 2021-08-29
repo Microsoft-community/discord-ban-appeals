5718a97999725201859652a98f3586b930d83175
Erisa A <erisa@erisa.uk>
2021-08-29 22:57:06 +0100

    test

--- api/oauth.js
+++ api/oauth.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event, context) {
-    const redirectUri = new URL("/api/oauth-callback", VERCEL_URL);
+    const redirectUri = new URL("/api/oauth-callback", process.env.VERCEL_URL);
     let url = `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(process.env.DISCORD_CLIENT_ID)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify&prompt=none`;
 
     if (event.queryStringParameters.state !== undefined) {
