feddcf1ce64df007e8346bdb5ee6edc4ec9b6c8a
Nikhil B <heftymausnik@gmail.com>
2022-02-28 19:57:40 +0530

    Revert "Server-side CSRF checks from upstream"
    This reverts commit e5e4bb0d16a957d16a69a11d1d7a077ccc3a26bd.

--- api/oauth.js
+++ api/oauth.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import crypto from "crypto";
-
 export default async (req, res) => {
 	if(req.method !== "GET") {
 		res.status(405).send("Invalid method");
@@ -14,19 +12,17 @@ export default async (req, res) => {
 	}
 
 	const redirectURI = `https://${req.headers.host}/api/oauth-callback`;
-	const state = crypto.randomBytes(25).toString("hex");
 	const params = {
 		client_id: process.env.DISCORD_CLIENT_ID,
 		redirect_uri: redirectURI,
 		response_type: "code",
 		scope: "identify",
-		prompt: "none",
-		state: state
+		prompt: "none"
 	}
 	if (req.query.state) {
 		params.state = req.query.state;
 	}
 	const authURL = `https://discord.com/api/oauth2/authorize?${new URLSearchParams(params).toString()}`;
 
-	res.setHeader("Set-Cookie", `__Secure-CSRFState=${state}; Domain=${req.headers.host}; Path=/api/oauth-callback; Secure; HttpOnly; SameSite=Lax`).redirect(303, authURL);
+	res.redirect(303, authURL);
 }
