4befce1e12dfd5f53d6b1a2ebb5505287eb1e0ed
Nikhil B <heftymausnik@gmail.com>
2022-02-28 20:19:35 +0530

    Trying again

--- api/oauth.js
+++ api/oauth.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import crypto from "crypto";
+
 export default async (req, res) => {
 	if(req.method !== "GET") {
 		res.status(405).send("Invalid method");
@@ -12,17 +14,19 @@ export default async (req, res) => {
 	}
 
 	const redirectURI = `https://${req.headers.host}/api/oauth-callback`;
+	const state = crypto.randomBytes(25).toString("hex");
 	const params = {
 		client_id: process.env.DISCORD_CLIENT_ID,
 		redirect_uri: redirectURI,
 		response_type: "code",
 		scope: "identify",
-		prompt: "none"
+		prompt: "none",
+		state: state
 	}
 	if (req.query.state) {
 		params.state = req.query.state;
 	}
 	const authURL = `https://discord.com/api/oauth2/authorize?${new URLSearchParams(params).toString()}`;
 
-	res.redirect(303, authURL);
+	res.setHeader("Set-Cookie", `__Secure-CSRFState=${state}; Domain=${req.headers.host}; Path=/api/oauth-callback; Secure; HttpOnly; SameSite=Lax`).redirect(303, authURL);
 }
