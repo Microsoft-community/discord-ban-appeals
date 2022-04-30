001dd84bdd560a0dcb0111a590cb1ccc3e5a2648
Nikhil B <heftymausnik@gmail.com>
2022-04-30 21:37:00 +0530

    Trying server-side CSRF again

--- api/oauth.js
+++ api/oauth.js
@@ -1,28 +1,29 @@
 "use strict";
 
+import crypto from "crypto";
+
 export default async (req, res) => {
 	if(req.method !== "GET") {
 		res.status(405).send("Invalid method");
 		return;
 	}
 
-	if (!req.headers.host) {
-		res.status(400);
-		return;
-	}
-	
 	const redirectURI = `https://${req.headers["x-forwarded-host"]}/api/oauth-callback`;
+	const state = crypto.randomBytes(25).toString("hex");
+	console.log(state)
 	const params = {
 		client_id: process.env.DISCORD_CLIENT_ID,
 		redirect_uri: redirectURI,
 		response_type: "code",
 		scope: "identify",
-		prompt: "none"
+		prompt: "none",
+		state: state
 	}
 	if (req.query.state) {
 		params.state = req.query.state;
 	}
 	const authURL = `https://discord.com/api/oauth2/authorize?${new URLSearchParams(params).toString()}`;
+	console.log(authURL)
 
-	res.redirect(303, authURL);
+	res.setHeader("Set-Cookie", `__Secure-CSRFState=${state}; Domain=${req.headers["x-forwarded-host"]}; Path=/api/oauth-callback; Secure; HttpOnly; SameSite=Lax`).redirect(303, authURL);
 }
