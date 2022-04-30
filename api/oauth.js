a4f11163352056a8513031f53a1ead4ae20b385b
Nikhil B <heftymausnik@gmail.com>
2022-04-30 22:19:44 +0530

    Enforce node version

--- api/oauth.js
+++ api/oauth.js
@@ -10,7 +10,6 @@ export default async (req, res) => {
 
 	const redirectURI = `https://${req.headers["x-forwarded-host"]}/api/oauth-callback`;
 	const state = crypto.randomBytes(25).toString("hex");
-	console.log(state)
 	const params = {
 		client_id: process.env.DISCORD_CLIENT_ID,
 		redirect_uri: redirectURI,
@@ -23,7 +22,6 @@ export default async (req, res) => {
 		params.state = req.query.state;
 	}
 	const authURL = `https://discord.com/api/oauth2/authorize?${new URLSearchParams(params).toString()}`;
-	console.log(authURL)
 
 	res.setHeader("Set-Cookie", `__Secure-CSRFState=${state}; Domain=${req.headers["x-forwarded-host"]}; Path=/api/oauth-callback; Secure; HttpOnly; SameSite=Lax`).redirect(303, authURL);
 }
